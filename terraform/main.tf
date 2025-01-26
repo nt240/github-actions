e4261aa4cecda0c85336ba8d5875368502275f86
nt240 <tadashi.nishio@akane.waseda.jp>
2025-01-26 19:03:31 +0900

    Add initial Terraform configuration for GitHub repository and branch protection

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.5.0"
+    }
+  }
+}
+
+provider "github" {}
+
+resource "github_repository" "this" {
+  name        = "github-actions"
+  description = "A repository for GitHub Actions"
+
+  visibility = "private"
+  has_downloads = true
+  has_issues = true
+  has_projects = true
+  has_wiki = false
+  delete_branch_on_merge = true
+
+  auto_init = true
+}
+
+resource "github_branch_protection" "this" {
+  repository_id = github_repository.this.node_id
+  pattern          = "main"
+  enforce_admins   = true
+  allows_deletions = false
+}
