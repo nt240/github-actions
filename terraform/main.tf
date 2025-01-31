875133639fd51f19215947abb5de3b82e7bd0d40
nt240 <tadashi.nishio@akane.waseda.jp>
2025-02-01 03:02:19 +0900

    Change repository visibility to public and adjust formatting in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,18 +13,18 @@ resource "github_repository" "this" {
   name        = "github-actions"
   description = "A repository for GitHub Actions"
 
-  visibility = "private"
-  has_downloads = true
-  has_issues = true
-  has_projects = true
-  has_wiki = false
+  visibility             = "public"
+  has_downloads          = true
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = false
   delete_branch_on_merge = true
 
   auto_init = true
 }
 
 resource "github_branch_protection" "this" {
-  repository_id = github_repository.this.node_id
+  repository_id    = github_repository.this.node_id
   pattern          = "main"
   enforce_admins   = true
   allows_deletions = false
